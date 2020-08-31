343e7396dbd53eb2b7b6b2887dfba19fe840597f
philippvb <philippvonbachmann@web.de>
2020-08-31 17:28:14 +0100

    finished presentation

--- paper/images/average.py
+++ paper/images/average.py
@@ -3,20 +3,20 @@ import os
 import pandas as pd
 
 def average():
-    directory = 'paper/images/network_csv/ensemble/MobileNetV2/'
-    networkname= 'mobileNetV2_ensemble_distance' # 'ResNet32_strength_e1'2
+    directory = 'paper/images/network_csv/baseline/MobileNetV2/'
+    networkname= 'mobileNetV2_baseline_long' # 'ResNet32_strength_e1'2
     #out_directory = 'paper/images/network_csv/baseline/MobileNetV2/'
     newName = 'MobileNetV2_ensemble_distance' 
 
 
     tags=[
-    'Ensemble_accuracy'#,
+    'Validation_accuracy'#,
     #'distance0',
 
  ]
 
     out_names=[
-        directory + newName + '_Ensemble_accuracy.csv'#,
+        directory + newName + '_validation_acuracy.csv'#,
         #directory + newName + '_distance0.csv'
     ]
 
@@ -24,14 +24,14 @@ def average():
         file_dir = directory + 'run-' + networkname
         filename_1=file_dir + '_1-tag-' + tag + '.csv'
         filename_2=file_dir + '_2-tag-' + tag + '.csv'
-       # filename_3=file_dir + '_3-tag-' + tag + '.csv'
+        filename_3=file_dir + '_3-tag-' + tag + '.csv'
     
     
         file1 = pd.read_csv(filename_1, delimiter=',')
         file2 = pd.read_csv(filename_2, delimiter=',')
-        #file3 = pd.read_csv(filename_3, delimiter=',')
-        file_sum= file1 + file2 #+ file3
-        file_sum = file_sum/2
+        file3 = pd.read_csv(filename_3, delimiter=',')
+        file_sum= file1 + file2 + file3
+        file_sum = file_sum/3
         # file_sum['Value'] = file_sum['Value'].ewm(alpha=0.4, axis=0, adjust=False).mean() for smoothing
         file_sum.to_csv(out_name, index=False)
     
